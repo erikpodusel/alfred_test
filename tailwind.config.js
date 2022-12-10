f0d8dbabd7aa932f4ed25c41ef8dc0eb6b3dde13
Erik Podušel <podusel@gomerch.company>
2022-12-10 16:38:04 +0100

    Project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        light: "rgba(90, 114, 123, 0.11) 0px 7px 30px 0px",
+      },
+    },
+    fontFamily: {
+      montserrat: "Montserrat, sans-serif",
+    },
+  },
+  plugins: [],
+}
