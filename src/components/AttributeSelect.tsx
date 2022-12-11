a8e7cdfa3dc2ce68d217c7047847c0b785f36a53
Erik Podušel <podusel@gomerch.company>
2022-12-11 22:11:26 +0100

    removed unused forgotten props

--- src/components/AttributeSelect.tsx
+++ src/components/AttributeSelect.tsx
@@ -6,12 +6,11 @@ interface Props {
   value: string | undefined;
 }
 
-export const AttributeSelect: FC<Props> = ({ value, onSelect, attributes }, props) => {
+export const AttributeSelect: FC<Props> = ({ value, onSelect, attributes }) => {
   return (
     <div className={"col w-full sm:w-[unset]"}>
       <label htmlFor="Attribute">Attribute</label>
       <select
-        {...props}
         className={"input"}
         value={value}
         onChange={({ target: { value } }) => onSelect(value)}
